e22b6cb88e8b0291d7988bdfc8687fa6e88071dc
Paulo Peixoto <jsfoliveira89@gmail.com>
2022-08-05 10:50:22 -0300

    Inserida uma logo, centralizado o conteúdo no container e arredondado o input

--- App.js
+++ App.js
@@ -1,31 +1,50 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { View, StyleSheet, TextInput, Image } from "react-native";
 
 const App = () => {
   const [text, onChangeText] = React.useState("Buscar pokemon");
 
 
   return (
-    <SafeAreaView>
+    <View style={styles.container}>
+      <Image
+        alt="logo"
+        source={{
+          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1920px-International_Pok%C3%A9mon_logo.svg.png',
+        }}
+        style={styles.logo}
+      />
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
         value={text}
       />
 
-    </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 60,
+    paddingVertical:50,
+  },
+
   input: {
     textAlign:"center",
     height: 40,
-    marginVertical: 100,
-    marginHorizontal: 500,
+    marginVertical: 50,
     borderWidth: 1,
     padding: 10,
+    borderRadius: 20,
   },
+
+  logo: {
+    textAlign:"center",
+    width: 280,
+    height: 100,
+  }
+
 });
 
 export default App;
