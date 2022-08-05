b604a1d139ca6e12e9bfc352a788893d1d3f9d89
Paulo Peixoto <jsfoliveira89@gmail.com>
2022-08-05 10:21:37 -0300

    Adicionada imagem da logo e input

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
-import { Text, View } from 'react-native';
+const App = () => {
+  const [text, onChangeText] = React.useState("Buscar pokemon");
 
-export default function App() {
+
   return (
-    <View>
-      <Text>consegui</Text>
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+
+    </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  input: {
+    textAlign:"center",
+    height: 40,
+    marginVertical: 100,
+    marginHorizontal: 500,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default App;
